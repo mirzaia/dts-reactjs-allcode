6e8395d7de38bb815c64b31b59ca23e26e072679
Mirza Aziz <mirz1723@gmail.com>
2022-05-25 20:32:55 +0700

    meeting 5 - array,object,map,set

---
+++ index.js
@@ -0,0 +1,120 @@
+// DEKLARASI ARRAY
+/*  const arr = [100, 200, 300];
+
+const arr2 = [];
+
+arr2[0] = 100;
+arr2[1] = 200;
+arr2[2] = 300;
+
+arr2.push(400);
+arr2.push("ini string");
+
+console.log(arr);
+console.log(arr2); */
+
+
+
+// NESTED ARRAY
+/* const barisDanKolom = [];
+
+barisDanKolom[0] = [];
+barisDanKolom[1] = [];
+barisDanKolom[2] = [];
+
+barisDanKolom[0][0] = 100;
+barisDanKolom[0][1] = 200;
+barisDanKolom[0][2] = 300;
+
+barisDanKolom[1][0] = 400;
+barisDanKolom[1][1] = 500;
+barisDanKolom[1][2] = 600;
+
+barisDanKolom[2][0] = 700;
+barisDanKolom[2][1] = 800;
+barisDanKolom[2][2] = 900;
+
+console.log(barisDanKolom);
+
+
+const arrGuru = [];
+const arrMurid = [];
+const arrSekuriti = [];
+
+const roster = [];
+
+arrGuru.push("Andre", "Bima", "Charles");
+arrMurid.push("Dodit", "Echa", "Fina");
+arrSekuriti.push("Gareng", "Hilmi", "Ina");
+
+roster.push(Guru, Murid, Sekuriti);
+
+console.log(roster);
+console.log(arrSekuriti.sort());
+console.log(arrMurid.join("-"));
+*/
+
+
+
+
+// OBJECT
+/* const kucing = {
+// props: value
+    mata: "Biru",
+    kaki: 4,
+    bulu: "Orange",
+
+};
+
+const kucingSatu = new Object();
+
+kucingSatu.mata = "Coklat";
+kucingSatu.kaki = 4;
+kucingSatu.bulu = "Hitam";
+
+
+console.log(kucing);
+console.log(kucingSatu);
+*/
+
+/*
+// MAP
+let firstMap = new Map();
+const station = {
+    zone: "JKT",
+    id: 1111,
+    status: true
+};
+
+firstMap.set("Driver Bill", "iPhone XS");
+firstMap.set(130539, "allow_job_reattempt");
+firstMap.set(station, "Jakarta");
+
+console.log(firstMap.get(station));
+*/
+
+
+
+// SET
+
+let firstSet = new Set();
+
+let setSatu = {
+    nama: "Tan"
+};
+
+let setDua = {
+    nama: "Jun"
+};
+
+let setTiga = {
+    nama: "Don"
+};
+
+firstSet.add(setSatu);
+firstSet.add(setDua);
+firstSet.add(setTiga);
+
+console.log(firstSet);
+console.log(firstSet.size);
+
